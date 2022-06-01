d9f217e4a74740401a517040c11fdcdd096df9e9
NouemanKHAL <noueman.khal@gmail.com>
2022-06-01 16:29:02 +0200

    add prepare, publish and release scripts

---
+++ scripts/release.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+RELEASE_TYPE=${RELEASE_TYPE:-"minor"}
+
+# prepare resources
+./scripts/prepare.sh
+
+cd tile/
+
+# get next semversion
+LATEST_VERSION=$(tail -n1 tile-history.yml | awk '{print $2}')
+SPLIT_VERSION=(${LATEST_VERSION//./ })
+
+major=${SPLIT_VERSION[0]}
+minor=${SPLIT_VERSION[1]}
+patch=${SPLIT_VERSION[2]}
+
+if [ "$RELEASE_TYPE" == "major" ]; then
+    major=$(expr $major + 1)
+    minor="0"
+    patch="0"
+elif [ "$RELEASE_TYPE" == "minor" ]; then
+    minor=$(expr $minor + 1)
+    patch="0"
+elif [ "$RELEASE_TYPE" == "patch" ]; then
+    patch=$(expr $patch + 1)
+else
+    echo "you must specify a valid RELEASE_TYPE. (major, minor, patch)"
+    exit 1
+fi
+
+VERSION="$major.$minor.$patch"
+
+# install dependency
+pip install tile-generator
+
+# build product file
+tile build $VERSION
